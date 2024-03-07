a6467266fc8c7909e603a11fb12c803313353b41
LaraBiewers <larabiewers@gmail.com>
2024-03-07 09:39:30 +0100

    raycast with viasual projectile

--- Unity_Dateien/Killing Bill/Assets/Skripts/Enemy/DetectColisionAntibody.cs
+++ Unity_Dateien/Killing Bill/Assets/Skripts/Enemy/DetectColisionAntibody.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class DetectColision : MonoBehaviour
+public class DetectColisionAntibody : MonoBehaviour
 {
 
     private GameManager gameManager;
@@ -16,18 +16,10 @@ public class DetectColision : MonoBehaviour
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        Destroy(other.gameObject);
     }
-
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    Destroy(gameObject);
-    //    gameManager.updateScore(100);
-    //    Destroy(other.gameObject);
-    //}
 
     public void TakeDamage (float amount)
     {
--- Unity_Dateien/Killing Bill/Assets/Skripts/Weapon/WeaponController.cs
+++ Unity_Dateien/Killing Bill/Assets/Skripts/Weapon/WeaponController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour    
 {
-
+    // References
     public Camera fpsCam;
-    public GameObject projectilePrefab;
-
+    public Transform waeponOutput;
     private AudioSource gunAudio;
 
-    public float fireRate = 0.25f;
-    public float damage = 1f;
+    // bullet
+    public GameObject projectilePrefab;
 
+    // bullet force
+    public float shootForce;
+
+    // shooting characteristics
+    public float damage = 1f;
+    public float fireRate = 0.25f;
     private float nextFire;
 
     // Start is called before the first frame update
@@ -24,10 +29,13 @@ public class WeaponController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Launch a projectile from the player
+        // Transform WeaponPosition = transform.parent;
+
+        // Launch a projectile from player
         if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
             Shoot();
+            // GameObject projectile = Instantiate(projectilePrefab, transform.position, WeaponPosition.rotation);
         }
     }
 
@@ -36,21 +44,42 @@ public class WeaponController : MonoBehaviour
         gunAudio.Play();
         nextFire = Time.time + fireRate;
 
-        //Transform parentTransform = transform.parent;
-        //GameObject projectile = Instantiate(projectilePrefab, transform.position, parentTransform.rotation);
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Just a ray through the middle of current view
 
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit))
+        Vector3 targetPoint;
+
+        // check if ray hits something
+        if (Physics.Raycast(ray, out hit))
         {
+            // Logging what's hit and where
             Debug.Log(hit.transform.name);
+            targetPoint = hit.point;
 
-            DetectColision target = hit.transform.GetComponent<DetectColision>();
+            // hit Antibody -> inflict damage
+            DetectColisionAntibody target = hit.transform.GetComponent<DetectColisionAntibody>();
 
-            if(target != null)
+            if (target != null)
             {
                 target.TakeDamage(damage);
             }
         }
+        else
+        {
+            targetPoint = ray.GetPoint(75); //Just a point far away from the player
+        }
+
+        // direction from attackPoint to targetPoint
+        Vector3 direction = targetPoint - waeponOutput.position;
+
+        // Instantiate bullet/projectile
+        GameObject currentProjectile = Instantiate(projectilePrefab, waeponOutput.position, Quaternion.identity); 
+        currentProjectile.transform.forward = direction.normalized;
+
+        // Add forces to bullet
+        currentProjectile.GetComponent<Rigidbody>().AddForce(direction.normalized * shootForce, ForceMode.Impulse);
+
+        // Currently not-existing effects :3
 
     }
 }
